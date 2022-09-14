580953c42c702e53a07234b571a7354542974ba5
Brendon Crave <brendoncrave@devpipeline.com>
2022-09-14 22:26:20 +0400

    Revert "C - feature C"
    This reverts commit b155379616b007a3f1d4d159863992f2c606ef00.

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ function preventDefault() {
   event.preventDefault();
 }
 
+function setActiveTab(el) {
+  var tabs = document.getElementsByClassName("tab");
+  for (var tab of tabs) {
+    tab.classList.remove("tab-active");
+  }
+  el.classList.add("tab-active");
+}
+
 // add click listener to each tab
 var tabs = document.getElementsByClassName("tab");
 for (var tab of tabs) {
