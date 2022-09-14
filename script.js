190671a3ebe43540e3d3d209097afe4800b6a4c7
Brendon Crave <brendoncrave@devpipeline.com>
2022-09-14 22:28:56 +0400

    Revert "A - feature A"
    This reverts commit a3a2c996086a5aed29e99472274fad897bb46431.

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 function preventDefault() {
-  event.preventDefault();
+    event.preventDefault();
 }
 
-function setActiveTab(el) {
-  var tabs = document.getElementsByClassName("tab");
-  for (var tab of tabs) {
-    tab.classList.remove("tab-active");
-  }
-  el.classList.add("tab-active");
+function setActiveContent(el) {
+    var contentBodies = document.getElementsByClassName('content-body');
+
+    // remove .show-active from all elements
+    for (var contentBody of contentBodies) {
+        contentBody.classList.remove('show-active');
+    }
+    // add .show-active back to clicked element
+    document.
+        getElementById(el.textContent.trim())
+        .classList.add('show-active');
+}
+
+function setActiveTab(el){
+    var tabs = document.getElementsByClassName('tab');
+    for (var tab of tabs) {
+        tab.classList.remove('tab-active');
+    }
+    el.classList.add('tab-active');
 }
 
 // add click listener to each tab
-var tabs = document.getElementsByClassName("tab");
+var tabs = document.getElementsByClassName('tab');
 for (var tab of tabs) {
-  tab.addEventListener("click", function (e) {
-    setActiveContent(e.currentTarget);
-    setActiveTab(e.currentTarget);
-  });
+    tab.addEventListener('click', function(e) {
+        setActiveContent(e.currentTarget);
+        setActiveTab(e.currentTarget);
+    })
 }
 
-let quantityValue = parseInt(document.querySelector(".input-quantity").value);
 
-document.querySelector(".plus").addEventListener("click", () => {
-  quantityValue = quantityValue + 1;
-  document.querySelector(".input-quantity").value = `${quantityValue}`;
-});
-document.querySelector(".minus").addEventListener("click", () => {
-  if (quantityValue > 1) {
-    quantityValue = quantityValue - 1;
-  }
-  document.querySelector(".input-quantity").value = `${quantityValue}`;
-});
+
+let quantityValue = parseInt(document.querySelector('.input-quantity').value) 
+
+document.querySelector('.plus').addEventListener('click',()=>{
+    quantityValue = quantityValue + 1
+    document.querySelector('.input-quantity').value = `${quantityValue}`
+})
+document.querySelector('.minus').addEventListener('click',()=>{
+    if(quantityValue>1){
+        quantityValue = quantityValue - 1
+    }
+    document.querySelector('.input-quantity').value = `${quantityValue}`
+})
